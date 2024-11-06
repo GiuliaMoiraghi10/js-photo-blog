07cd0b2522fb7ae64df7218bdf7a6808ad463d42
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2024-11-06 15:30:29 +0100

    Create card html & css

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log('JS-PHOTO-BLOG')
+
+
+// card
+/* <div class="col-4">
+    <div class="card">
+        <img class="pin" src="./img/pin.svg" alt="">
+        <img class="jpeg" src="./img/Vulpix.full.1515167.webp" alt="">
+        <p>Lorem ipsum dolor sit amet consectetur adipisic.</p>
+    </div>
+</div> */
+

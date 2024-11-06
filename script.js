c5ce4ef4607df6a17211739dc47b9cc1e7f8e736
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2024-11-06 15:59:01 +0100

    Procedimento teorico JS

--- script.js
+++ script.js
@@ -10,3 +10,13 @@ console.log('JS-PHOTO-BLOG')
     </div>
 </div> */
 
+// PROCEDIMENTO (Generico)
+// 1) Fare chiamata ajax a json per recuperare 6 foto e 6 titoli
+// 2) Quando arriva risposta, genero html delle card con foto e titoli e li metto nel DOM
+
+// PROCEDIMENTO (Specifico)
+// 1) Prendere row dove andranno colonne e card html (.url e .title)
+// 2) Creo variabile con nome url completo
+// 3) Faccio chiamata axios per prendere url json
+// 4) Faccio funzione con ciclo for per generare 6 card
+// 5) Appendo le card all'html
